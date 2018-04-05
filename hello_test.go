7a422193725861063256a61be8efcae4b3213526
Weylin Walter Wagnon <weylinwagnon@gmail.com>
2018-04-05 15:59:14 -0700

    first commit

---
+++ hello_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	assertMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		assertMessage(t, Hello("Weylin"), "Hello, Weylin")
+	})
+	t.Run("say Hello, World when empty string is supplied", func(t *testing.T){
+		assertMessage(t, Hello(""), "Hello, World")
+	})
+}
